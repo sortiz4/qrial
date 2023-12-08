86e6a0ee40f6b094a5369828ae59da6f65e07183
Steven Ortiz <sortiz832@gmail.com>
2023-12-07 23:42:29 -0600

    Refactored script

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,4 @@
-import qrFromImage from 'jsqr';
+import jsQr, { QRCode as QrCode } from 'jsqr';
 import fs from 'node:fs';
 import path from 'node:path';
 import { PNG as Png } from 'pngjs';
@@ -18,117 +18,114 @@ interface Options {
   readonly deserialize: boolean;
 }
 
-class QrCode {
-  constructor(public name: string, public text: string) {
+function deserialize(files: string[], output: string): void {
+  function fromJson(file: string): Schema[] {
+    return JSON.parse(fs.readFileSync(file, 'utf-8'));
   }
 
-  static fromJson(filePath: string): QrCode[] {
-    return (JSON.parse(fs.readFileSync(filePath, 'utf-8')) as Schema[]).map(code => new this(code.name, code.data));
-  }
-
-  static fromPng(filePath: string): QrCode {
-    const name = path.basename(filePath);
-    const png = Png.sync.read(fs.readFileSync(filePath));
-    const qr = qrFromImage(new Uint8ClampedArray(png.data), png.width, png.height);
-    return new this(name, qr?.data ?? '');
-  }
-
-  static tryFromJson(filePath: string): QrCode[] | void {
+  function tryFromJson(file: string): Schema[] | undefined {
     try {
-      return this.fromJson(filePath);
+      return fromJson(file);
     } catch {
       // Return undefined
     }
   }
 
-  static tryFromPng(filePath: string): QrCode | void {
-    try {
-      return this.fromPng(filePath);
-    } catch {
-      // Return undefined
-    }
+  function isSchemaArray(value?: Schema[]): value is Schema[] {
+    return value instanceof Array;
+  }
+
+  const codes = files.map(tryFromJson).filter(isSchemaArray).flat();
+
+  // Resolve the output
+  const destination = output ? output : 'schema';
+
+  // Make the directory
+  fs.mkdirSync(destination, { recursive: true });
+
+  // Deserialize the data
+  for (const code of codes) {
+    qrToFile(path.join(destination, code.name), code.data, { type: 'png', scale: 8 });
   }
 }
 
-function getCommandOptions(): Options {
-  const deserializeOptions: YargsOptions = {
-    alias: 'deserialize',
-    boolean: true,
-    default: false,
-    describe: 'Deserializes a JSON schema into (PNG) QR codes',
-  };
+function serialize(files: string[], output: string): void {
+  function imageToQr(data: Uint8ClampedArray, width: number, height: number): QrCode {
+    return (jsQr as unknown as Function)(data, width, height);
+  }
 
-  const serializeOptions: YargsOptions = {
-    alias: 'serialize',
-    boolean: true,
-    default: false,
-    describe: 'Serializes (PNG) QR codes into a JSON schema',
-  };
+  function fromPng(file: string): Schema {
+    const png = Png.sync.read(fs.readFileSync(file));
+    const qr = imageToQr(new Uint8ClampedArray(png.data), png.width, png.height);
 
-  const outputOptions: YargsOptions = {
-    alias: 'output',
-    string: true,
-    default: '',
-    describe: 'Where file(s) produced by this tool should be saved',
-  };
+    return {
+      name: path.basename(file),
+      data: qr?.data ?? '',
+    };
+  }
 
-  return (
-    yargs(process.argv.slice(2))
-      .scriptName(metadata.name)
-      .version(metadata.version)
-      .help()
-      .wrap(100)
-      .usage('Usage: $0 [-s|-d] [-o PATH] [FILES]')
-      .option('d', deserializeOptions)
-      .option('s', serializeOptions)
-      .option('o', outputOptions)
-      .alias({ h: 'help', v: 'version' })
-      .argv as unknown as Options
-  );
+  function tryFromPng(file: string): Schema | undefined {
+    try {
+      return fromPng(file);
+    } catch {
+      // Return undefined
+    }
+  }
+
+  function isSchema(value?: Schema): value is Schema {
+    return !!value;
+  }
+
+  // Serialize the data
+  fs.writeFileSync(output ? output : 'schema.json', JSON.stringify(files.map(tryFromPng).filter(isSchema)));
 }
 
 function main(): void {
+  function getCommandOptions(): Options {
+    const deserializeOptions: YargsOptions = {
+      alias: 'deserialize',
+      boolean: true,
+      default: false,
+      describe: 'Deserializes a JSON schema into (PNG) QR codes',
+    };
+
+    const serializeOptions: YargsOptions = {
+      alias: 'serialize',
+      boolean: true,
+      default: false,
+      describe: 'Serializes (PNG) QR codes into a JSON schema',
+    };
+
+    const outputOptions: YargsOptions = {
+      alias: 'output',
+      string: true,
+      default: '',
+      describe: 'Where file(s) produced by this tool should be saved',
+    };
+
+    return (
+      yargs(process.argv.slice(2))
+        .scriptName(metadata.name)
+        .version(metadata.version)
+        .help()
+        .wrap(100)
+        .usage('Usage: $0 [-s|-d] [-o PATH] [FILES]')
+        .option('d', deserializeOptions)
+        .option('s', serializeOptions)
+        .option('o', outputOptions)
+        .alias({ h: 'help', v: 'version' })
+        .argv as unknown as Options
+    );
+  }
+
   const options = getCommandOptions();
 
   if (options.deserialize) {
-    const codes = (
-      options._
-        .map(filePath => QrCode.tryFromJson(filePath))
-        .filter(item => item instanceof Array)
-        .flat()
-    );
+    deserialize(options._, options.output);
+  }
 
-    // Resolve the output
-    const output = options.output.length > 0 ? (
-      options.output
-    ) : (
-      'schema'
-    );
-
-    // Make the directory
-    fs.mkdirSync(output, { recursive: true });
-
-    // Deserialize the data
-    for (const code of codes) {
-      qrToFile(path.join(output, code?.name ?? ''), code?.text ?? '', { type: 'png', scale: 8 });
-    }
-  } else if (options.serialize) {
-    const schema = (
-      options._
-        .map(filePath => QrCode.tryFromPng(filePath))
-        .filter(item => item instanceof QrCode)
-        .map(code => ({ name: code?.name, data: code?.text }))
-    );
-
-    // Resolve the output
-    const output = options.output.length > 0 ? (
-      options.output
-    ) : (
-      'schema.json'
-    );
-
-    // Serialize the data
-    fs.writeFileSync(output, JSON.stringify(schema));
+  if (options.serialize) {
+    serialize(options._, options.output);
   }
 }
 
